f63e16e3802366faba9f81ef52a763a5a331fc67
Mzn97 <mazen.karim97@yahoo.com>
2023-12-25 23:30:25 +0100

    Kompletterat js & html - Kalkylera kostnaderna

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buyButtons = document.querySelectorAll('.buyButton');
     const cart = document.getElementById('cartItems');
+    let totalCost = 0;
 
     buyButtons.forEach(function(buyButton) {
         buyButton.addEventListener('click', function() {
+
+            buyButton.disabled = true;
+
             const songContainer = buyButton.closest('.song-details');
             const songTitle = songContainer.querySelector('p').textContent;
+            const songPrice = parseFloat(songContainer.querySelector('p').getAttribute('data-price'));
 
-            const purchasedSong = document.createElement('li');
-            purchasedSong.textContent = songTitle;
+            if (!isNaN(songPrice)) {
+                const purchasedSong = document.createElement('li');
+                purchasedSong.textContent = songTitle;
 
-            cart.appendChild(purchasedSong);
+                cart.appendChild(purchasedSong);
 
-            alert('Låten har lagts till i din korg.');
+                totalCost += songPrice;
+                updateTotalCost();
+
+                alert('Låten har lagts till i din korg.');
+            } else {
+                alert('Felaktig prisinformation.');
+            }
+
+            setTimeout(function() {
+                buyButton.disabled = false;
+            }, 1000);
         });
     });
+
+    function updateTotalCost() {
+        document.getElementById('totalCost').textContent = `Total kostnad: ${totalCost.toFixed(2)} kr`;
+    }
 });
